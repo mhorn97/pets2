89bbd1ea7c170c385f368498800adbc6beb0ae5d
Tony <tony.g.thompson@gmail.com>
2018-01-18 10:57:00 -0800

    push it real good

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 
 require_once ('vendor/autoload.php');
 
+// Turn on error reporting
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
 $f3 = Base::instance();
 
 //Set debug level
@@ -15,5 +19,35 @@ $f3->set('DEBUG', 3);
 
 $f3 -> route('GET /', function() {
     echo "<h1>Routing Demo</h1>";
+
 }
 );
+
+
+$f3 -> route('GET /pets/show/@pet', function($f3, $params) {
+    switch($params['pet']) {
+        case 'cat':
+            echo 'Cat!'; break;
+            echo '<img src="smiley.gif" alt="cat" height="42" width="42">';
+        case 'bigcat':
+            echo 'BIGCAT!'; break;
+        case 'notcat':
+            echo 'Not a cat!'; break;
+        //404 error default
+        default:
+            $f3->error(404);
+    }
+
+}
+);
+
+$f3 -> route('GET /pets/order', function() {
+    $template = new Template();
+    echo $template->render('views/home.html');
+}
+);
+
+
+
+//Run Fat-Free Framework
+$f3->run();
