178b606ebe31d73965ff6a61211b1b254c080db9
Michael Horn <michaelhorn1@hotmail.com>
2018-01-18 13:33:13 -0500

    Trying to define a default route

--- index.php
+++ index.php
@@ -5,3 +5,15 @@
  * Date: 1/18/2018
  * Time: 10:29 AM
  */
+
+require_once ('vendor/autoload.php');
+
+$f3 = Base::instance();
+
+//Set debug level
+$f3->set('DEBUG', 3);
+
+$f3 -> route('GET /', function() {
+    echo "<h1>Routing Demo</h1>";
+}
+);
