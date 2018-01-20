db451c7dd94ae52986c2e18fdc185a343eeeea1f
Tony <tony.g.thompson@gmail.com>
2018-01-19 21:55:13 -0800

    final fix

--- index.php
+++ index.php
@@ -28,16 +28,19 @@ $f3 -> route('GET /', function() {
 );
 
 
-$f3 -> route('GET /pets/show/@pet', function($f3, $params) {
+$f3 -> route('GET /show/@pet', function($f3, $params) {
     switch($params['pet']) {
         case 'cat':
-            echo 'Cat!'; break;
-            echo '<img src="https://static.pexels.com/photos/127028/pexels-photo-127028.jpeg" alt="cat" height="42" width="42">';
+            echo 'Cat!';
+            echo '<img src="https://static.pexels.com/photos/127028/pexels-photo-127028.jpeg" alt="cat">';
+            break;
         case 'bigcat':
-            echo 'BIGCAT!'; break;
-            echo '<img src="https://img.buzzfeed.com/buzzfeed-static/static/2014-04/enhanced/webdr06/4/16/enhanced-10419-1396642697-10.jpg" alt="cat" height="42" width="42">';
+            echo 'BIGCAT!';
+            echo '<img src="https://img.buzzfeed.com/buzzfeed-static/static/2014-04/enhanced/webdr06/4/16/enhanced-10419-1396642697-10.jpg" alt="cat">';
+            break;
         case 'notcat':
-            echo 'Not a cat!'; break;
+            echo 'Not a cat!';
+            break;
         //404 error default
         default:
             $f3->error(404);
@@ -52,17 +55,19 @@ $f3 -> route('GET /order', function() {
 }
 );
 
-$f3 -> route('POST /order2', function($f3,$params) {
-    $f3->set('pet',$params['pet']);
-    $_SESSION['pet'] = $f3->get('pet');
+$f3 -> route('POST /order2', function() {
+    $_SESSION['pet'] = $_POST['pet'];
     $template = new Template();
     echo $template->render('views/form2.html');
 }
 );
 
-$f3 -> route('POST /results', function($f3,$params) {
-    $f3 -> set('color',$params['color']);
-    $_SESSION['color'] = $f3->get('color');
+$f3 -> route('POST /results', function($f3) {
+
+    $_SESSION['color'] = $_POST['color'];
+    $f3->set('pet', $_SESSION['pet']);
+    $f3 -> set('color', $_SESSION['color']);
+
     $template = new Template();
     echo $template->render('views/results.html');
 });
